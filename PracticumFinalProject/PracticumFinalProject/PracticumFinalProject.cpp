7c5cd40b83c718c604fae96ec19442bda21f2407
Amer Pandzharov <amerpandzharov@gmail.com>
2024-12-16 01:42:56 +0200

    Added basics

---
+++ PracticumFinalProject/PracticumFinalProject/PracticumFinalProject.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+
+using namespace std;
+
+const int MAX_SIZE = 100;
+
+enum ActionTypesEnum
+{
+    Additon = '+',
+    Subtraction = '-',
+    Division = '/',
+    Multiplication = 'x'
+};
+
+struct Cell {
+    int value;
+
+    ActionTypesEnum actionType;
+};
+
+struct Player
+{
+    int x;
+    int y;
+
+    double totalSum = 0;
+
+    bool used = false;
+};
+
+struct Board
+{
+    Cell cells[MAX_SIZE][MAX_SIZE];
+
+    int Rows;
+    int Cols;
+};
+
+struct Game {
+    
+};
+
+int main()
+{
+    
+}
